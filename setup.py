079aea22ec99e30dced2acda4f4a69328026244e
kirby@puppetlabs.com <mk.kirby@gmail.com>
2016-08-15 15:31:29 -0700

    Re-arrange so module name is valid

--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='py-vmware',
-    version='0.0.1',
+    name='py_vmware',
+    version='0.0.2',
     packages=find_packages(),
     install_requires=['pyvmomi'],
     entry_points={
         'console_scripts': [
-            'vmware_clone_vm = py-vmware.clone_vm:main',
-            'vmware_check_cluster = py-vmware.cluster_host_health:main',
-            'vmware_destroy_vm = py-vmware.destroy_vm:main',
-            'vmware_getvms = py-vmware.getallvms:main',
-            'vmware_host_maintenance = py-vmware.migrate_host_vms:main',
-            'vmware_migrate_vm_datastore = py-vmware.migrate_vm_datastore:main',
-            'vmware_vm_snapshot = py-vmware.vm_snapshot:main',
+            'vmware_clone_vm = py_vmware.clone_vm:main',
+            'vmware_check_cluster = py_vmware.cluster_host_health:main',
+            'vmware_destroy_vm = py_vmware.destroy_vm:main',
+            'vmware_getvms = py_vmware.getallvms:main',
+            'vmware_host_maintenance = py_vmware.migrate_host_vms:main',
+            'vmware_migrate_vm_datastore = py_vmware.migrate_vm_datastore:main',
+            'vmware_vm_snapshot = py_vmware.vm_snapshot:main',
         ]
     },
     author='Matt Kirby',

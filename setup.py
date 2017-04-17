42d2fa2cc6771995dae4f8d0a65e37f22b69bb68
kirby@puppetlabs.com <mk.kirby@gmail.com>
2017-04-17 16:08:09 -0700

    Bump version to .16

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='py_vmware',
-    version='0.0.15',
+    version='0.0.16',
     packages=find_packages(),
     install_requires=['pyvmomi'],
     entry_points={

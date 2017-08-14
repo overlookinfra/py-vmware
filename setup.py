ffaad0a81065ea7735f5be6a24834300cd004d4b
kirby@puppetlabs.com <mk.kirby@gmail.com>
2017-08-14 14:35:29 -0700

    Only use slot for provider management
    This commit reverts thread assignment for check_pool to a per-pool basis. Without this change a set of threads is shared in a manner that is not guaranteed to be thread safe.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='py_vmware',
-    version='0.0.16',
+    version='0.0.17',
     packages=find_packages(),
     install_requires=['pyvmomi'],
     entry_points={
